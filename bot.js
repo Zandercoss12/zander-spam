8fceee717c2e750437a93ee2306262d605ce7b69
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-02-22 20:33:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ client.on("message", async message => {
   if (command === "spam") {
   	for (int i = 0; i < 100; i++) {
   		message.channel.sendMessage("@Zandercross12#7714");
-  		break;
   	}
+  break;
   }
 
 });
